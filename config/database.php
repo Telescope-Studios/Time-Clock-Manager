a40d2c0d16187bf050f4b6323d6d8b5d1d83b54e
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2020-04-20 22:30:08 -0500

    Heroku using DSN

--- config/database.php
+++ config/database.php
@@ -37,11 +37,13 @@ return [
 
         'mongodb' => [
             'driver' => 'mongodb',
-            'host' => env('DB_HOST'),
+            'dsn' => env('DB_DSN'),
+            'database' => env('DB_DATABASE'),
+            /*'host' => env('DB_HOST'),
             'port' => env('DB_PORT', 27017),
             'database' => env('DB_DATABASE'),
             'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD'),
+            'password' => env('DB_PASSWORD'),*/
             'options' => [
             // here you can pass more settings to the Mongo Driver Manager
             // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
