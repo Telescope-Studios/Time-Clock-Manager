a5298397664a45dbf08a254401171cfdc0719e31
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2020-04-20 22:26:22 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -37,11 +37,11 @@ return [
 
         'mongodb' => [
             'driver' => 'mongodb',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST'),
             'port' => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE', 'tcm'),
-            'username' => env('DB_USERNAME', 'mongoadmin'),
-            'password' => env('DB_PASSWORD', 'mongoadmin'),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
             'options' => [
             // here you can pass more settings to the Mongo Driver Manager
             // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
