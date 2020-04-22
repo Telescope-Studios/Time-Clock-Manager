78f7b79aaff5ecade18f948db098c7a7a3a13265
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2020-04-22 17:20:37 -0500

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -38,6 +38,7 @@ Vue.component('employee-table-component', EmployeeTableComponent);
 Vue.component('job-table-component', JobTableComponent);
 Vue.component('report-table-component', ReportTableComponent);
 Vue.component('timesheet-component', TimesheetComponent);
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
